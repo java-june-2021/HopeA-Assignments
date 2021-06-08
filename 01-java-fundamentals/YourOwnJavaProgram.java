abfa6c5baf79adb8d00de2dec43a413aa152f98b
HAnderson9933 <79612554+HAnderson9933@users.noreply.github.com>
2021-06-08 16:42:55 -0600

    Your Own Java Program file

---
+++ 01-java-fundamentals/YourOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class YourOwnJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Hope");
+        System.out.println("I am 39 years old");
+        System.out.println("My hometown is Layton, UT");
+    }
+}
