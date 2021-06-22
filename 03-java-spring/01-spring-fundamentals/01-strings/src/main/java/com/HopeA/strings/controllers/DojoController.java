06f36352fa4b0f8ceb71aae4e463b4edd98ec1e0
HAnderson9933 <79612554+HAnderson9933@users.noreply.github.com>
2021-06-22 16:20:45 -0600

    Strings File

---
+++ 03-java-spring/01-spring-fundamentals/01-strings/src/main/java/com/HopeA/strings/controllers/DojoController.java
@@ -0,0 +1,26 @@
+package com.HopeA.strings.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DojoController {
+	@RequestMapping("/dojo")
+	public String dojo() {
+		return "The dojo is awesome!";
+	}
+	
+	@RequestMapping("/dojo/{location}")
+	public String dojoSchool(@PathVariable ("location") String location) {
+		switch(location) {
+		case "burbank":
+			return "Burbank Dojo is located in Southern California";
+		case "san-jose":
+			return "SJ dojo is the headquarters";
+		}
+		return location;
+	}
+
+}
